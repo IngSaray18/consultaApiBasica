bb5f5e1b7e2f881e35456c22a4cdbcb5b1712ec0
IngSaray18 <pedrosaray18@hotmail.com>
2023-09-12 17:05:53 -0600

    .1

--- src/components/Confirmacion.jsx
+++ src/components/Confirmacion.jsx
@@ -1,92 +1,77 @@
-import React, { useContext,useState,useEffect } from 'react';
-import styled from 'styled-components';
-import { ContextoProcesos } from '../context/ContextoProcesos';
-import axios from 'axios';
-import Modular from './Modular';
+import React, { useContext, useState, useEffect } from "react";
+import styled from "styled-components";
+import { ContextoProcesos } from "../context/ContextoProcesos";
+import axios from "axios";
+import Modular from "./Modular";
 function ConfirmationPage() {
+  const { procesos } = useContext(ContextoProcesos);
 
-const { procesos } = useContext(ContextoProcesos)
- 
-useEffect(() => {
-  
+  useEffect(() => {}, []);
+  let endpoint = "";
+  let nombreProceso = "";
 
-}, []);
-let endpoint = "" 
-let nombreProceso =""
+  const [loading, setLoading] = useState(false);
 
-const [loading, setLoading] = useState(false);
-
-const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleStartProcess = async () => {
+    setOpen(true);
 
-        setOpen(true);
-
-       
-
-      console.log(procesos);
-     if (procesos.includes("RESPALDO VENTANILLA") ) {
-        nombreProceso = "Respaldo Ventanilla"
-       endpoint = "backup/backup-database"
-     } else if (procesos.includes("BATCH VENTANILLA")) {
-      nombreProceso= "Batch Ventanilla"
-       endpoint = "ejecutar/ejecutar-code"
+    console.log(procesos);
+    if (procesos.includes("RESPALDO VENTANILLA")) {
+      nombreProceso = "Respaldo Ventanilla";
+      endpoint = "backup/backup-databaseVentanillaWeb";
+    } else if (procesos.includes("BATCH VENTANILLA")) {
+      nombreProceso = "Batch Ventanilla";
+      endpoint = "ejecutar/ejecutar-code";
+    } else if (procesos.includes("RESPALDO  BD Seguridad")) {
+      nombreProceso = "Respaldo BD_Seguridad";
+      endpoint = "backup/backup-databaseBDSeg";
+    } else if (procesos.includes("RESPALDO CAJAAHORRO")) {
+      nombreProceso = "Respaldo RESPALDO CAJAAHORRO";
+      endpoint = "backup/backup-databaseCajaAhorro";
+    } else if (procesos.includes("RESPALDO CAJAAHORROT")) {
+      nombreProceso = "Respaldo RESPALDO CAJAAHORRO T";
+      endpoint = "backup/backup-databaseCajaAhorroT";
     }
 
     try {
-       setLoading(true); // Activar el loader
-       // Hacer la solicitud GET al servidor
-       const response = await axios.get(`http://localhost:3000/${endpoint}`  );
-      
-       // Manejar la respuesta exitosa aquí
-       console.log(response.data);
+      setLoading(true); // Activar el loader
+      // Hacer la solicitud GET al servidor
+      const response = await axios.get(`http://localhost:4000/${endpoint}`);
 
+      // Manejar la respuesta exitosa aquí
+      console.log(response.data);
     } catch (error) {
-       // Manejar errores aquí
-      console.error('Error al hacer la solicitud:', error);
-     }finally {
-    setLoading(false); // Desactivar el loader después de la solicitud
+      // Manejar errores aquí
+      console.error("Error al hacer la solicitud:", error);
+    } finally {
+      setLoading(false); // Desactivar el loader después de la solicitud
     }
-
-
-
-
-  }
-
-
-  
+  };
 
   const handleBack = () => {
-    history.back()
-    };
+    history.back();
+  };
 
   return (
     <Container>
-       { open?
+      {open ? (
+        <Modular loader={loading} nombreProceso={nombreProceso} open={open} />
+      ) : (
+        <ul>
+          {procesos.map((proceso, index) => {
+            return <li key={index}>{proceso}</li>;
+          })}
+        </ul>
+      )}
 
-          <Modular loader={loading} nombreProceso={nombreProceso} open={open}/> 
-         :
-
-                  
-         <ul>
-         { procesos.map( (proceso, index)=>{
-           return <li key={index}>{proceso}</li>
-         }  ) }
-       </ul>
-        
-      }
-     
       <ButtonContainer>
         <Button secondary onClick={handleBack}>
           Regresar
         </Button>
         <Button onClick={handleStartProcess}>Comenzar proceso</Button>
-
-      
       </ButtonContainer>
-      
-        
-
     </Container>
   );
 }
@@ -104,7 +89,7 @@ const ButtonContainer = styled.div`
 
 const Button = styled.button`
   padding: 10px 20px;
-  background-color: ${(props) => (props.secondary ? '#ccc' : '#007bff')};
+  background-color: ${(props) => (props.secondary ? "#ccc" : "#007bff")};
   color: white;
   border: none;
   border-radius: 5px;
